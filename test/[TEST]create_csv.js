d334f4858ce6c4bd68f77df58848f23baa44892b
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2024-06-04 20:51:50 +0300

    fixed games.csv

--- test/[TEST]create_csv.js
+++ test/[TEST]create_csv.js
@@ -3,13 +3,17 @@ const fs = require('fs');
 const data = require("../games/process_mapping.json");
 const sortedKeys = Object.keys(data).sort();
 
-let csvContent = "Executable Names,Game Name,Keywords\n";
+let csvContent = "Executable Names,Game Name,Keyword 1,Keyword 2,Keyword 3,Keyword 4\n";
 
 sortedKeys.forEach(key => {
     const values = data[key];
     const exe = key;
     const gameName = values[0];
-    const keywords = values.slice(1, -1).join(', ');
-    csvContent += `${exe},${gameName},${keywords}\n`;
+    const keywords = values.slice(1, -1);
+    while (keywords.length < 4) {
+        keywords.push("-");
+    }
+    csvContent += `${exe},${gameName},${keywords[0]},${keywords[1]},${keywords[2]},${keywords[3]}\n`;
 });
+
 fs.writeFileSync('games.csv', csvContent, 'utf8');
